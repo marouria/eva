d12b24c75112d63407d05529ff80d7eb39c27e37
Marine Dominé <marine@captive.fr>
2021-10-01 12:27:52 +0200

    WIP - Bienvenue - Ajoute les questions audio

--- src/situations/questions/vues/acte.vue
+++ src/situations/questions/vues/acte.vue
@@ -9,6 +9,11 @@
         @reponse="repondQuestion"
         :envoyerEvenementAffichage="acteEnCours"
       >
+        <bouton-son
+          class="bouton-son"
+          :idQuestion="questionCourante.id"
+          :question="questionCourante"
+        />
         <div class="question-progression">
           {{ indexQuestions + 1 }}/{{ nombreQuestions }}
         </div>
@@ -26,13 +31,13 @@ import QuestionQcm from 'commun/vues/qcm';
 import QuestionRedactionNote from './redaction_note';
 import 'questions/styles/progression.scss';
 import { DEMARRE } from 'commun/modeles/situation';
+import BoutonSon from './bouton-son.vue';
 
 export default {
-  components: { TransitionFade },
-
+  components: { TransitionFade, BoutonSon },
   computed: {
     ...mapState(['indexQuestions', 'etat', 'fini']),
-    ...mapGetters(['questionCourante', 'nombreQuestions']),
+    ...mapGetters(['questionCourante', 'nombreQuestions', 'audioQuestion']),
 
     composantQuestion () {
       if (!this.questionCourante) return;
